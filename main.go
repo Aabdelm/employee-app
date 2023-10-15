9ea0c3e65da550ee4d35263efe72a9838ee8644b
Abdullah Abdelmagid <abdullanezar19@gmail.com>
2023-10-15 15:15:28 -0400

    Import go/chi Add new routers and handling from chi

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+
+	employeedb "github.com/Aabdelm/employee-app/database"
+	"github.com/Aabdelm/employee-app/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -13,13 +18,37 @@ func main() {
 		For the time being, os.stdout will be used
 		A file will be used later
 	*/
-	l := log.New(os.Stdout, "employee-app:", log.LstdFlags)
-	s := &http.Server{
-		Addr: "localhost:80",
-	}
+	l := log.New(os.Stdout, "", log.LstdFlags)
+
+	router := chi.NewRouter()
+	//Method router for usage in tandem with current logger
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
 	//Create a signal channel to detect interrupts and/or shutdowns
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	//Create and setup the database
+	db := employeedb.NewDbMap(l)
+	if err := employeedb.SetupDb(db); err != nil {
+		l.Print("ERROR: FAILED TO CONNECT TO DATABASE", err)
+	}
+
+	defer db.Db.Close()
+
+	employeeHandler := handlers.NewEmployeHandler(l, db)
+	deptHandler := handlers.NewDepartmentHandler(l, db)
+
+	router.Get("/employees/{id}", employeeHandler.GetEmployee)
+	router.Post("/employees/{id}", employeeHandler.AddEmployee)
+
+	router.Post("/departments/{id}", deptHandler.PostDepartment)
+
+	s := &http.Server{
+		Addr:    "localhost:80",
+		Handler: router,
+	}
 
 	//run goroutine
 	go func() {
@@ -27,7 +56,12 @@ func main() {
 		s.ListenAndServe()
 
 	}()
+
+	//sig is blocking
+	//go routine should run here until one of the two signals are recieved
 	x := <-sig
+
+	//shutdown
 	l.Println("Signal Recieved:", x)
 	l.Println("Attempting shutdown")
 
