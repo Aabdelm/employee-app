7a0800627e33153c72f0d75ee8bdcc0038ffd12c
Abdullah Abdelmagid <abdullanezar19@gmail.com>
2023-12-23 15:13:33 -0500

    Add new methods for get/put/delete

--- handlers/departments.go
+++ handlers/departments.go
@@ -11,13 +11,13 @@ import (
 )
 
 type DepartmentHandler struct {
-	l     *log.Logger
-	DbMap *employeedb.DbMap
+	L     *log.Logger
+	DbMap employeedb.DeptMapper
 }
 
-func NewDepartmentHandler(l *log.Logger, DbMap *employeedb.DbMap) DepartmentHandler {
+func NewDepartmentHandler(l *log.Logger, DbMap employeedb.DeptMapper) DepartmentHandler {
 	return DepartmentHandler{
-		l:     l,
+		L:     l,
 		DbMap: DbMap,
 	}
 }
@@ -27,34 +27,112 @@ func (dh DepartmentHandler) PostDepartment(rw http.ResponseWriter, r *http.Reque
 
 	dept := employeedb.NewEmployeeDepartment()
 
-	s := chi.URLParam(r, "id")
-
-	paramId, err := strconv.Atoi(s)
-	if err != nil {
-		dh.l.Printf("[ERROR] Failed to convert string. Error: %s\n", err)
-		rw.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	dept.Id = paramId
-
 	j := json.NewDecoder(r.Body)
 
 	if err := j.Decode(dept); err != nil {
-		dh.l.Printf("[ERROR] Failed to parse json. Error: %s\n", err)
-		rw.WriteHeader(http.StatusInternalServerError)
+		dh.L.Printf("[ERROR] Failed to parse json. Error: %s\n", err)
+		http.Error(rw, "Failed to decode json", http.StatusBadRequest)
 		return
 	}
 
-	dh.l.Printf("[INFO] Decoded json: ID %d\n Department: %s\n", dept.Id, dept.Department)
+	dh.L.Printf("[INFO] Decoded json: ID %d\n Department: %s\n", dept.Id, dept.Department)
 
 	err = dh.DbMap.AddNewDepartment(dept)
 
 	if err != nil {
-		dh.l.Println("[ERROR] Failed to insert department")
-		rw.WriteHeader(http.StatusBadRequest)
+		dh.L.Println("[ERROR] Failed to insert department")
+		http.Error(rw, "Failed to insert department", http.StatusBadRequest)
 		return
 	}
-	dh.l.Printf("[INFO] Added new department %s", dept.Department)
+	dh.L.Printf("[INFO] Added new department %s", dept.Department)
+
+}
+
+func (dh DepartmentHandler) PutDepartment(rw http.ResponseWriter, r *http.Request) {
+	var err error
+
+	idStr := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to convert id to integer. Error %s", err)
+		http.Error(rw, "Failed to convert id to integer", http.StatusBadRequest)
+		return
+	}
+
+	dec := json.NewDecoder(r.Body)
+	newDept := employeedb.NewEmployeeDepartment()
+
+	err = dec.Decode(newDept)
+
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to decode JSON. Error %s", err)
+		http.Error(rw, "Failed to convert JSON. Data might be malformed", http.StatusBadRequest)
+		return
+	}
+
+	err = dh.DbMap.UpdateDepartment(id, newDept, newDept.Department)
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to update department %d. Error %s", id, err)
+		http.Error(rw, "Failed to update department", http.StatusInternalServerError)
+		return
+	}
+
+	dh.L.Printf("Updated department %d", id)
+
+}
+
+func (dh DepartmentHandler) DeleteDepartment(rw http.ResponseWriter, r *http.Request) {
+	var err error
+
+	idS := chi.URLParam(r, "id")
+	id, err := strconv.Atoi(idS)
+
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to parse int from URL. Error %s", err)
+		http.Error(rw, `Failed to parse int from URL. 
+		This might be due to the parameter being a string`, http.StatusBadRequest)
+		return
+	}
+
+	err = dh.DbMap.RemoveDepartment(id)
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to delete id %d", id)
+		http.Error(rw, "Failed to delete department", http.StatusInternalServerError)
+		return
+	}
+
+	dh.L.Printf("[INFO] Deleted id %d", id)
+}
+
+func (dh DepartmentHandler) GetDepartment(rw http.ResponseWriter, r *http.Request) {
+	var err error
+	idStr := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to convert URL parameter to integer. Error %s", err)
+		http.Error(rw, `Failed to convert URL parameter to integer. 
+		Your URL might contain a string rather than an integer`, http.StatusBadRequest)
+		return
+	}
+
+	emps, err := dh.DbMap.GetEmployeesByDepartment(id)
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to get employee department. Error %s", err)
+		http.Error(rw, "Failed to retrieve employee department", http.StatusInternalServerError)
+		return
+	}
+
+	enc := json.NewEncoder(rw)
+	err = enc.Encode(emps)
+
+	if err != nil {
+		dh.L.Printf("[ERROR] Failed to encode JSON for employees. Error %s", err)
+		http.Error(rw, "Failed to encode JSON for employees", http.StatusInternalServerError)
+		return
+	}
+	dh.L.Printf("[INFO] Successfully retrieved Employeed for dept %d", id)
 
 }
