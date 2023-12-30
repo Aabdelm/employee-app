6e08a5f3dcb92c516f9f0410c9dc8e3633840b89
Abdullah Abdelmagid <abdullanezar19@gmail.com>
2023-12-30 13:39:59 -0500

    Improve logging messages

--- database/employees.go
+++ database/employees.go
@@ -22,7 +22,7 @@ func (DbMap *DbMap) GetEmployeeById(id int) (*Employee, error) {
 	row := statement.QueryRow(id)
 
 	if err != nil {
-		DbMap.l.Printf("[ERROR] Query failed. error:%s\n", err)
+		DbMap.l.Printf("[ERROR] Query failed. In function GetEmployeeById. error:%s\n", err)
 		return nil, err
 	}
 
@@ -40,6 +40,8 @@ func (DbMap *DbMap) GetEmployeeById(id int) (*Employee, error) {
 
 func (DbMap *DbMap) UpdateEmployee(employee *Employee) error {
 	var err error
+	DbMap.l.Println("Function UpdateEmployee called")
+
 	if err = DbMap.disableForeignKeyChecks(); err != nil {
 		return err
 	}
@@ -51,7 +53,7 @@ func (DbMap *DbMap) UpdateEmployee(employee *Employee) error {
 	`)
 
 	if err != nil {
-		DbMap.l.Printf("[ERROR] Failed to prepare statement. %s\n", err)
+		DbMap.l.Printf("[ERROR] Failed to prepare statement in function UpdateEmployee. Error %s\n", err)
 		return err
 	}
 
@@ -61,7 +63,7 @@ func (DbMap *DbMap) UpdateEmployee(employee *Employee) error {
 		employee.DepartmentId, time.Now(), employee.Id, employee.DepartmentId)
 
 	if err != nil {
-		DbMap.l.Printf("[ERROR] Failed to execute statement. %s\n", err)
+		DbMap.l.Printf("[ERROR] Failed to execute statement in function UpdateEmployee. Error %s\n", err)
 		return err
 	}
 
@@ -69,17 +71,18 @@ func (DbMap *DbMap) UpdateEmployee(employee *Employee) error {
 		return err
 	}
 
-	DbMap.l.Printf("Successfully updated employee %d\n", employee.Id)
+	DbMap.l.Printf("[INFO] Successfully updated employee %d\n", employee.Id)
 
 	return nil
 }
 
 // delete employee removes the employee off the database
 func (DbMap *DbMap) DeleteEmployee(id int) error {
+	DbMap.l.Println("[INFO] Function Delete Employee called")
 	statement, err := DbMap.Db.Prepare(`DELETE FROM employee
 	WHERE id = ?`)
 	if err != nil {
-		DbMap.l.Printf("[ERROR] Failed to execute query for id %d. Error: %s\n", id, err)
+		DbMap.l.Printf("[ERROR] Failed to execute query for id %d in Function DeleteEmployee. Error: %s\n", id, err)
 		return err
 	}
 
@@ -97,6 +100,9 @@ func (DbMap *DbMap) DeleteEmployee(id int) error {
 
 func (DbMap *DbMap) AddNewEmployee(employee *Employee) error {
 	var err error
+
+	DbMap.l.Println("[INFO] Function AddNewEmployee called")
+
 	statement, err := DbMap.Db.Prepare(`INSERT INTO employee
 	 (first_name, last_name, email, department_id, date_added, date_modified) 
 	 VALUES (?,?,?,?,?,?)`)
