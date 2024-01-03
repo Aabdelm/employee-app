ea057cd516136d3ab7048c0cec0005cafc78aac3
Abdullah Abdelmagid <abdullanezar19@gmail.com>
2024-01-03 16:57:45 -0500

    Improve error handling with toast notifications Add function awaitAndToastify

--- static/apis.js
+++ static/apis.js
@@ -2,7 +2,8 @@
     Methods here are responsible for API methods
 */
 
-import { renderNewEmployee, renderExistingEmployee, removeDeletedEmployees, renderDepartmentAddition } from "./script.js";
+import { renderNewEmployee, renderExistingEmployee, 
+    removeDeletedEmployees, renderDepartmentAddition, clearAndReRender, toastify } from "./script.js";
 
 //Master method for methods
 export async function submitEmployee(employee, Method){
@@ -14,7 +15,7 @@ export async function submitEmployee(employee, Method){
         case 'POST':
             const newEmp = await postEmployee(employee)
             //Hand it over to the table for new rendering
-            renderNewEmployee(newEmp)
+            renderNewEmployee(newEmp, "POST")
             break;
     }
 }
@@ -28,7 +29,10 @@ export async function deleteEmployees(employees){
             },
             body: JSON.stringify(employees),
         });
-        removeDeletedEmployees();
+        if(!response.ok){
+            awaitAndToastify(response);
+        }
+        else removeDeletedEmployees();
     }catch(e){
         console.error(e);
     }
@@ -43,10 +47,15 @@ async function postEmployee(employee){
             },
             body: JSON.stringify(employee)
         });
-        const emp = await response.json();
+        if(!response.ok){
+            awaitAndToastify(response);
+        }else{
+            const emp = await response.json();
+            //post employee for the frontend
+            return emp;
+        }
 
-        //post employee for the frontend
-        return emp;
+        
     }catch(e){
         console.error(e);
     }
@@ -61,8 +70,12 @@ async function putEmployee(employee){
         },
         body: JSON.stringify(employee),
     });
-        const updatedEmp = await response.json();
-        return updatedEmp
+        if(!resp.ok){
+            awaitAndToastify(response);
+        }else{
+            const updatedEmp = await response.json();
+            return updatedEmp
+        }
     }catch(e){
         console.error(e);
     }
@@ -77,8 +90,62 @@ export async function submitDepartment(dept){
             },
             body: JSON.stringify(dept),
         })
-        renderDepartmentAddition();
+        if(!res.ok){
+            awaitAndToastify(res);
+        }
+        else renderDepartmentAddition();
     }catch(e){
         console.error(e)
     }
+}
+
+export async function query(identifier, value){
+    
+    const query = value.split("").filter(char => char != '-').join("");
+    console.log(value);
+    if(value == 'employee-id' && !isDigit(identifier)) return;
+
+    try{
+        const resp = value !== 'employee-id' ? 
+        await fetch(`http://localhost:80/employees?${query}=${identifier}`) : 
+        await fetch(`http://localhost:80/employees/${identifier}`)
+        if (!resp.ok){
+            awaitAndToastify(resp);
+        }else{
+            const emps = value !== 'employee-id' ? [...await resp.json()] : [await resp.json()];
+            clearAndReRender(emps);
+        }
+        
+    }catch(e){
+        console.error(e)
+    }
+}
+
+const isDigit = (digit) =>{
+    const regEx = new RegExp("^[0-9]+$");
+    return regEx.test(digit);
+}
+
+export async function getEmployeeById(stringId){
+    //Strictly filter for digits
+    const reg = new RegExp("^[0-9]+$");
+    if(!reg.test(stringId)) return;
+
+    try{
+        const resp = await fetch(`http://localhost:80/employees/${stringId}`)
+        if(!resp.ok){
+            awaitAndToastify(resp);
+        }
+        else{
+            const id = await resp.json();
+            clearAndReRender(id);
+        }
+    }catch(e){
+        console.error(e);
+    }
+}
+
+export async function awaitAndToastify(res){
+    const err = await res.text();
+    toastify(err);
 }
